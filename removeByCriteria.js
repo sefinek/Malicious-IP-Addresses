40da258e4e1d3d187af3964f7141095b6c9761fc
Sefinek <contact@sefinek.net>
2025-04-07 21:22:25 +0200

    -1 lines from main.txt | -1 lines from details.csv

--- removeByCriteria.js
+++ removeByCriteria.js
@@ -68,7 +68,7 @@ const removeByCriteria = async (criteria, criteriaType) => {
 	// await removeByCriteria('', 'userAgent');
 
 	// Remove by IP
-	await removeByCriteria('70.50.0.92', 'ip');
+	await removeByCriteria('', 'ip');
 
 	// Remove by endpoint
 	// await removeByCriteria('', 'endpoint');
